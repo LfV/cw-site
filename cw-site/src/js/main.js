5d628d8c7ce8ceac0e5e6f10e0f73da705fe587a
LVinAction <lvcre8z@gmail.com>
2025-05-29 12:17:17 -0400

    Made updates at home

--- cw-site/src/js/main.js
+++ cw-site/src/js/main.js
@@ -1,11 +1,15 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
 
 // Import only the Bootstrap components we need
-import { Popover } from 'bootstrap';
+//import { Popover } from 'bootstrap';
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new Popover(popover)
-  })
+// Import all of Bootstrap’s JS
+import * as bootstrap from "bootstrap";
+
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
